cf7e14b80a3e89110ebddf2e458b263a1a0775b8
nelli-belovol <nelli199020@gmail.com>
2024-08-07 15:29:46 +0300

    changing requirements refactoring

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,8 @@
 export const dummyPasswords = ['12345678', 'password', 'Qwerty123'];
+
+export const passwordValidationErrors = {
+  length: 'Password must be at least 8 characters long',
+  case: 'Password must contain both upper and lower case letter',
+  number: 'Password must contain at least one digit',
+  special: 'Password must contain at least one special character',
+};
